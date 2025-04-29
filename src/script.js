4824b2bf31c1eb194d2cec95993de05d9c7bf693
CynthiaMugo <cynthiamugo25@gmail.com>
2025-04-29 20:34:12 +0300

    Script to validate reg form and return alert when successful

--- src/script.js
+++ src/script.js
@@ -1,8 +1,16 @@
 // Registration form for index.html - welcome user to the group
-document.getElementById("join-us").addEventListener("Submit", function(event) {
+document.getElementById("join-us").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    const name = document.getElementById("name").ariaValueMax.trim;
-    const email = document.getElementById("name").ariaValueMax.trim;
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const number = document.getElementById("number").value.trim();
+
+    const user = { name, email, number }
+    localStorage.setItem("fictionAddictUser", JSON.stringify(user));
+
+    alert("Thanks for registering. Welcome to Fiction Addicts!");
+    event.target.reset()
+
 });
 const form = document.getElementById("join-us");
