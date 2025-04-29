04c617653ccc2cd9c697c7ea213690d515f17cb7
CynthiaMugo <cynthiamugo25@gmail.com>
2025-04-29 20:23:16 +0300

    Link index.js to script.js for reg form

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// Registration form for index.html - welcome user to the group
+document.getElementById("join-us").addEventListener("Submit", function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").ariaValueMax.trim;
+    const email = document.getElementById("name").ariaValueMax.trim;
+});
+const form = document.getElementById("join-us");
